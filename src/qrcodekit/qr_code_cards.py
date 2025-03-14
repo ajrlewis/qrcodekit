aefb8679705166c96b9d9a6c5ea64bbbea58a8b8
ajrlewis <hello@ajrlewis.com>
2025-03-14 19:26:42 +0000

    Added create_nostr_card.

--- src/qrcodekit/qr_code_cards.py
+++ src/qrcodekit/qr_code_cards.py
@@ -70,3 +70,40 @@ def create_wifi_card(
 #             image, f"Amount: {amount}", font_size=15, font_color=(128, 128, 128)
 #         )
 #     return image
+
+
+def create_nostr_card(npub: str):
+    front, back = card(color=NOSTR_PURPLE)
+    width, height = front.size
+
+    # Logo on front
+
+    logo = read(f"{ASSETS_PATH}/nostr-logo-with-text.png")
+    logo_width, logo_height = logo.size
+    scale_factor = height * 0.8 / logo_height
+    logo = shrink(logo, scale_factor)
+    logo_width, logo_height = logo.size
+    origin = (width - logo_width) // 2, (height - logo_height) // 2
+    front.paste(logo, origin, mask=logo)  # Use alpha channel of logo for mask
+    qr = qrcode(f"nostr:{npub}", fill_color=WHITE, back_color=NOSTR_PURPLE)
+    scale_factor = height * 0.6 / qr.height
+    qr = shrink(qr, scale_factor)
+    qr_width, qr_height = qr.size
+    origin = (width - qr_width) // 2, int(round((height - qr_height) / 2.5))
+    logger.debug(f"{origin = }")
+    back.paste(qr, origin, mask=qr)  # Use alpha channel of logo for mask
+
+    # QRCode and npub on back
+
+    font_name = f"{ASSETS_PATH}/Ubuntu-Regular.ttf"
+    font_size = 26
+    font = ImageFont.truetype(font_name, size=font_size)
+
+    bbox = font.getbbox(npub)  # (left, top, right, bottom)
+    text_width = bbox[2] - bbox[0]
+    text_height = bbox[1] - bbox[3]
+    text_origin = (width - text_width) // 2, origin[1] + int(round(1.1 * qr_height))
+    draw = ImageDraw.Draw(back)
+    draw.text(text_origin, npub, font=font, fill=WHITE)
+
+    return front, back
